8105cefa1f25939cdb1b032a661a0fb5ebbec388
soojinahn <soojahnx@gmail.com>
2021-05-10 07:09:28 -0400

    Working on frontend

--- server.js
+++ server.js
@@ -1,36 +1,146 @@
+
+/**
+ * Required External Modules
+ */
+
 const express = require("express");
-const bodyParser = require("body-parser");
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+const authRouter = require("./auth");
 const cors = require("cors");
+const db = require("./app/models");
+
+/**
+ * App Variables
+ */
 
 const app = express();
+const port = process.env.PORT || "8000";
 
+/**
+ * Session Configuration
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+    db.sequelize.sync({ force: true }).then(() => {
+        console.log("Drop and re-sync db.");
+    });
+}
+
+// index.js
+
+/**
+ * Passport Configuration
+ */
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+/**
+ *  App Configuration
+ */
 var corsOptions = {
     origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession(session));
 
-const db = require("./app/models");
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
 
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
+passport.serializeUser((user, done) => {
+    done(null, user);
 });
 
-// simple route
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated();
+    next();
+});
+
+// Router mounting
+app.use("/", authRouter);
+
+/**
+ * Routes Definitions
+ */
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+};
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to my application." });
+    res.render("server", { title: "Home" });
 });
 
+app.get("/user", secured, (req, res, next) => {
+    const { _raw, _json, ...userProfile } = req.user;
+    res.render("user", {
+        title: "Profile",
+        userProfile: userProfile
+    });
+});
+
+db.sequelize.sync();
+
+/**
+ * Server Activation
+ */
 require("./app/routes/tutorial.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
 });
